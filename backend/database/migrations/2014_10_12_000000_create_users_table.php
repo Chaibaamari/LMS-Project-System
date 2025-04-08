dcb2d7deb53eee45d2479b22c720bf4eeb9f84f9
AbdnCH <132705059+AbdnCH@users.noreply.github.com>
2025-04-08 22:20:11 +0200

    added active

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('role', ['responsable','gestionnaire','consultant'])->default('consultant');
+            $table->boolean('active')->default(True);
             $table->timestamps();
         });
     }
