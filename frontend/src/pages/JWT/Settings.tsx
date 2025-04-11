0064d86fa62abeef3b877a53f6e7d1102113f014
Rayane <berragrayaneabdessalem@example.com>
2025-04-11 22:25:46 +0100

    rayane changes

---
+++ frontend/src/pages/JWT/Settings.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+    return (
+        <>
+            <h1 className="text-center">Settings</h1>
+            <p className='text-stone-400 text-center'>Welcome to the Settings page</p>
+        </>
+    );
+}
