409bc004cd6798ad9cb64c6afb6d52509e08e2ad
Chaiba Amari <chaibaamari2@example.com>
2025-05-26 16:11:48 +0100

    The Endab

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,20 +3,27 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create admin user if not exists
+        User::firstOrCreate(
+            ['email' => 'admin@sonatrach.dz'],
+            [
+                'name' => 'admin',
+                'password' => Hash::make('admin'),
+                'role' => 'responsable',
+                'active' => 1,
+            ]
+        );
     }
 }
